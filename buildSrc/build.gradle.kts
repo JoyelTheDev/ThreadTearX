6f5218658682133cde65773c50967c703fbf27c1
JoyelTheDev <222600183+JoyelTheDev@users.noreply.github.com>
2026-06-01 18:38:48 +0530

    Add Apache Ant dependency to build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     implementation(kotlin("gradle-plugin"))
     implementation("com.github.johnrengelman.shadow:com.github.johnrengelman.shadow.gradle.plugin:8.1.1")
+    implementation("org.apache.ant:ant:1.10.14")
 }
 
 repositories {
